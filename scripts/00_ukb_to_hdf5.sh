2f9040326c0c5adf3c05998d15dde52fcab433b1
Rui Costa <rui.costa@ebi.ac.uk>
2022-07-12 12:19:49 +0100

    correct file path

--- scripts/00_ukb_to_hdf5.sh
+++ scripts/00_ukb_to_hdf5.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bsub -J 'ukb_to_hdf5[1-503]' -o ukb_to_hdf5_out.txt -e ukb_to_hdf5_err.txt 
-'../../env/conda/envs/penv/bin/python3.9 00_ukb_to_hdf5.py $VAR1'
+
+bsub -J 'ukb_to_hdf5[1-503]' -o ukb_to_hdf5_out.txt -e ukb_to_hdf5_err.txt '/hps/software/users/gerstung/rui/env/conda/envs/penv/bin/python3.9 00_ukb_to_hdf5.py $VAR1'
