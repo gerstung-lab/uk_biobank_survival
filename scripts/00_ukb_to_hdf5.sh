44088de6c456af91434f85d246f1eefeca5c0b8b
Rui Costa <ruibarrigana@codon-login-01.ebi.ac.uk>
2022-07-12 11:54:03 +0100

    include job submission command

--- scripts/00_ukb_to_hdf5.sh
+++ scripts/00_ukb_to_hdf5.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-../../env/conda/envs/penv/bin/python3.9 00_ukb_to_hdf5.py $VAR1
+bsub -J 'ukb_to_hdf5[1-503]' -o ukb_to_hdf5_out.txt -e ukb_to_hdf5_err.txt 
+'../../env/conda/envs/penv/bin/python3.9 00_ukb_to_hdf5.py $VAR1'
