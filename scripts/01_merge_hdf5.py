6f5a9c9efc6e054d0809f88374f00ee1aa5353fa
Rui Costa <rui.costa@ebi.ac.uk>
2022-07-13 10:49:08 +0100

    change file paths

--- scripts/01_merge_hdf5.py
+++ scripts/01_merge_hdf5.py
@@ -7,10 +7,9 @@ import shutil
 import tqdm
 import h5py
 
-ROOT_DIR = '/nfs/research1/gerstung/sds/sds-ukb-cancer/'
 
-with h5py.File(ROOT_DIR + 'projects/MultiRisk/data/ukb.h5', 'w') as h5fw:
-    for h5name in tqdm.tqdm(glob.glob(ROOT_DIR + 'projects/MultiRisk/data/tmp/*.h5')):
+with h5py.File('../hdf5_files/ukb.h5', 'w') as h5fw:
+    for h5name in tqdm.tqdm(glob.glob('../hdf5_files/ukb_*.h5')):
         helpvar = list(h5fw.keys())
         h5fr = h5py.File(h5name,'r') 
         for obj in h5fr.keys():  
