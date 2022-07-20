9286ddb644c5752735679385df9b6caa64843ddb
Rui Costa <rui.costa@ebi.ac.uk>
2022-07-20 17:28:57 +0100

    parameter changes for model inference

--- scripts/03_inference.sh
+++ scripts/03_inference.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bsub -J 'inference_job' -e inference_err3.txt -o inference_out3.txt -n 12 -M 12000  '/hps/software/users/gerstung/rui/env/conda/envs/penv/bin/python3.9 03_inference.py' 
+bsub -J 'inference_job7' -e inference_err7.txt -o inference_out7.txt -n 12 -M 12000  '/hps/software/users/gerstung/rui/env/conda/envs/penv/bin/python3.9 03_inference.py' 
